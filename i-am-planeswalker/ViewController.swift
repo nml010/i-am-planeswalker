dd49af146309d13059d149a58d1b9d7bfec2d9cc
Nicholas <47710136+nml010@users.noreply.github.com>
2020-01-28 07:54:41 -0600

    Finished Ver 1.0
    Version 1.0 completed.

---
+++ i-am-planeswalker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i-am-planeswalker
+//
+//  Created by Nicholas Lee on 1/24/20.
+//  Copyright © 2020 Nicholas Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ i-am-planeswalkerTests/i_am_planeswalkerTests.swift
@@ -0,0 +1,34 @@
+//
+//  i_am_planeswalkerTests.swift
+//  i-am-planeswalkerTests
+//
+//  Created by Nicholas Lee on 1/24/20.
+//  Copyright © 2020 Nicholas Lee. All rights reserved.
+//
+
+import XCTest
+@testable import i_am_planeswalker
+
+class i_am_planeswalkerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
